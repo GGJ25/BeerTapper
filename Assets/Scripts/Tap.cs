258d2f05ef063a492a0c43d77df56969ff096f14
LuFrei <32626445+LuFrei@users.noreply.github.com>
2025-01-24 18:56:53 -0500

    Added Tap sprite, gave it animations, set up script to interact with other objects

---
+++ Assets/Scripts/Tap.cs
@@ -0,0 +1,32 @@
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class Tap : MonoBehaviour
+{
+
+    private Animator[] animators;
+    public bool openState;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start(){
+        animators = GetComponentsInChildren<Animator>();
+    }
+
+// Purely for testing, delete Update later
+    // void Update(){ 
+    //     foreach (Animator anim in animators)
+    //         anim.SetBool("isOpen", openState);
+    // }
+
+    void Open(){
+        openState = true;
+        foreach (Animator anim in animators)
+            anim.SetBool("isOpen", openState);
+    }
+
+    void Close(){
+        openState = false;
+        foreach (Animator anim in animators)
+            anim.SetBool("isOpen", openState);
+    }
+
+}
